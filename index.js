bade7788ca746dd707e7daed324646d203a2799b
munimrahman <munimrh@gmail.com>
2023-01-23 11:06:44 +0600

    database crud

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 const app = {};
+// testing file system
+// data.create('test', 'newFile', { name: 'Munim Rahman', age: 28 }, (err) => {
+//     console.log('Error: ', err);
+// });
 
+// data.update('test', 'newFile', { name: 'Shakib', age: 29 }, (err) => {
+//     console.log('Error: ', err);
+// });
+
+// data.read('test', 'newFile', (err, result) => {
+//     console.log(err, result);
+// });
+
+data.delete('test', 'newFile', (err) => {
+    console.log('Error: ', err);
+});
 app.config = {};
 
 app.createServer = () => {
